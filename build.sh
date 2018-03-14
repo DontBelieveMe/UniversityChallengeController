58ab0bd3aeb0a8e94dbfc2cf2abcc08fd78d9659
Barney Wilks <DontBelieveMe@users.noreply.github.com>
2018-03-14 21:52:08 +0000

    Updated comments in build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-# This is the main build file, because I cannot be bothered to write a proper makefile
 # To build the program use `sh build.sh` in the root directory
+# Run's CMake to generate the project files (aka makefiles most likely) and then uses CMake
+# to build the code.
 
 cmake -H. -Bbuild
 cmake --build build
